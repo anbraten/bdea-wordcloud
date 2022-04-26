a6e139a1dae2e08818f1ac3f330f19d5ce0dcfa8
Anbraten <anton@ju60.de>
2022-04-26 14:42:00 +0000

    :sparkles: allow txt uploads

--- src/route_upload.py
+++ src/route_upload.py
@@ -1,29 +1,22 @@
 import imghdr
 import os
 from flask import Flask, render_template, request, redirect, url_for, abort, \
-    send_from_directory
+    send_from_directory, redirect
 from werkzeug.utils import secure_filename
+import mimetypes
 
 import config
-
-def validate_image(stream):
-    header = stream.read(512)
-    stream.seek(0)
-    format = imghdr.what(None, header)
-    if not format:
-        return None
-    return '.' + (format if format != 'jpeg' else 'jpg')
 
 def upload():
     uploaded_file = request.files['file']
     filename = secure_filename(uploaded_file.filename)
     if filename != '':
         file_ext = os.path.splitext(filename)[1]
-        if file_ext not in config.get()['UPLOAD_EXTENSIONS'] or \
-                file_ext != validate_image(uploaded_file.stream):
-            return "Invalid image", 400
+        if file_ext not in config.get()['UPLOAD_EXTENSIONS'] or mimetypes.guess_type(filename)[0] != 'text/plain':
+            return "Invalid file format", 400
         uploaded_file.save(os.path.join(config.get()['UPLOAD_PATH'], filename))
-    return '', 204
+    return redirect("/", code=302)
+    # return '', 204
 
 def uploaded_files(filename):
     return send_from_directory(config.get()['UPLOAD_PATH'], filename)
