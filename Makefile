8c3a00bdbd4fdaee6f05c44e8cc473e2af7740c4
Anbraten <anton@ju60.de>
2022-04-27 12:12:35 +0000

    improve setup

--- Makefile
+++ Makefile
@@ -1,10 +1,17 @@
 MASTER := spark://spark:7077
+DOCKER_COMPOSE := docker-compose -f .devcontainer/docker-compose.yml
 
 .PHONY: all test coverage
 all: get build install
 
 wordcount: ## Run wordcount rdd
-	spark-submit --master $(MASTER) src/wordcount.py
+	spark-submit --master $(MASTER) spark/wordcount.py
+
+docker-up: ## Start docker setup
+	$(DOCKER_COMPOSE) up -d
+
+docker-bash: ## Start bash on app service
+	$(DOCKER_COMPOSE) exec app bash
 
 start: ## Start the webserver
 	FLASK_APP=src/server FLASK_ENV=development flask run --host=0.0.0.0
--- spark/wordcount.py
+++ spark/wordcount.py
@@ -6,6 +6,7 @@ basePath = 'file:///poor-hdfs/'
 spark = SparkSession.builder \
       .appName("wordcloud") \
       .master("spark://spark:7077") \
+      .config("spark.jars", "/poor-hdfs/postgresql-42.3.4.jar") \
       .getOrCreate()
 
 sc = spark.sparkContext
@@ -16,13 +17,26 @@ words = txt.flatMap(lambda line: re.split('\W+', line.lower()))
 words = words.filter(lambda x: len(x) > 3)
 wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
 sortedWordCounts = wordCounts.sortBy(lambda x: x[1])
-# sortedWordCounts.saveAsTextFile(basePath + "/output/")
 
 output = sortedWordCounts.collect()
 for (word, count) in output:
     print("%s: found %i times" % (word, count))
 
 print("Number of different words -> %i" % (sortedWordCounts.count()))
+
+# df = spark.createDataFrame(sortedWordCounts)
+# df.write.jdbc("")
+
+df = spark.read \
+    .format("jdbc") \
+    .option("url", "jdbc:postgresql://postgres:5432/postgres") \
+    .option("dbtable", "word-cloud") \
+    .option("user", "postgres") \
+    .option("password", "passw0rd") \
+    .option("driver", "org.postgresql.Driver") \
+    .load()
+
+df.printSchema()
 
 sc.stop()
 spark.stop()
