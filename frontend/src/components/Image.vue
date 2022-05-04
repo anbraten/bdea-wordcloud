7b16f17967cb48363559f71d02576f6fbce611e4
Anbraten <anton@ju60.de>
2022-05-04 08:56:25 +0000

    fix image

--- frontend/src/components/Image.vue
+++ frontend/src/components/Image.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col p-4 cursor-pointer border-1 border-gray-400 rounded-lg" @click="openImg">
     <div class="w-64 h-64 relative overflow-hidden">
-      <img :src="url" class="absolute w-full h-full bg-gray-400 object-cover" />
+      <img :src="imgUrl" class="absolute w-full h-full bg-gray-400 object-cover" />
     </div>
     <span>{{ name }}</span>
   </div>
